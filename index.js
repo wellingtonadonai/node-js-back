fe87f50b1637929b154df50aa89723eedc2a6662
wellington <wellington.h.s.oliveira@hotmail.com>
2023-12-16 16:41:54 -0300

    acresimo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
 import chalk from 'chalk';
+import fs from 'fs';
 
 
 
@@ -7,17 +7,35 @@ function trataErros (erro) {
     throw new Error (chalk.red(erro.code, 'nao ha arquivo no diretorio'));
 };
 
-function pegaArquivo (caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.readFile (caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro){
-        trataErros(erro);
-        };
-        console.log(chalk.green(texto));
-    });
-
+async function pegaArquivo (caminhoDoArquivo){
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.blue(texto))
+ } catch(erro){
+    trataErros(erro)
+ }
 }
+
+// function pegaArquivo (caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErros)
+// }
+
+// function pegaArquivo (caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile (caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro){
+//         trataErros(erro);
+//         };
+//         console.log(chalk.green(texto));
+//     });
+
+// }
 pegaArquivo('./texto.md')
+
 
 
 
